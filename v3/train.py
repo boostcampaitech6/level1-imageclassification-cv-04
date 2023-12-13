94f38afb15db8ce83a9fadd2b95de31b3ec5d4a6
solee328 <solee328@gmail.com>
2023-12-13 12:57:18 +0000

    feat: Initial commit V3
    close #14

--- v3/train.py
+++ v3/train.py
@@ -22,7 +22,7 @@ from loss import create_criterion
 def seed_everything(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     np.random.seed(seed)
