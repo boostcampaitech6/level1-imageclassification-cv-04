31e65ab994295553460b743ef3cfe96785e6b1c7
solee328 <solee328@gmail.com>
2023-12-14 04:09:00 +0000

    feat: modify train, trainer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,103 +1,319 @@
 import numpy as np
 import torch
+import os
+import random
+import glob
+import re
+import json
+from pathlib import Path
 from torchvision.utils import make_grid
-from base import BaseTrainer
-from utils import inf_loop, MetricTracker
+from base.base_trainer import BaseTrainer
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+# from utils import inf_loop, MetricTracker
 
+
+# class Trainer(BaseTrainer):
+#     """
+#     Trainer class
+#     """
+#     def __init__(self, model, criterion, metric_ftns, optimizer, config, device,
+#                  train_data_loader, valid_data_loader=None, lr_scheduler=None, len_epoch=None, multi_head=False, logger=None):
+#         super().__init__(model, criterion, metric_ftns, optimizer, config)
+#         self.config = config
+#         self.device = device
+#         self.data_loader = train_data_loader
+#         if len_epoch is None:
+#             # epoch-based training
+#             self.len_epoch = len(self.data_loader)
+#         else:
+#             # iteration-based training
+#             self.data_loader = inf_loop(train_data_loader)
+#             self.len_epoch = len_epoch
+#         self.valid_data_loader = valid_data_loader
+#         self.do_validation = self.valid_data_loader is not None
+#         self.lr_scheduler = lr_scheduler
+#         self.log_step = int(np.sqrt(train_data_loader.batch_size))
+
+#         self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
+#         self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
+
+#     def _train_epoch(self, epoch):
+#         """
+#         Training logic for an epoch
+
+#         :param epoch: Integer, current training epoch.
+#         :return: A log that contains average loss and metric in this epoch.
+#         """
+#         self.model.train()
+#         self.train_metrics.reset()
+#         for batch_idx, (data, target) in enumerate(self.data_loader):
+#             data, target = data.to(self.device), target.to(self.device)
+
+#             self.optimizer.zero_grad()
+#             output = self.model(data)
+#             loss = self.criterion(output, target)
+#             loss.backward()
+#             self.optimizer.step()
+
+#             self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
+#             self.train_metrics.update('loss', loss.item())
+#             for met in self.metric_ftns:
+#                 self.train_metrics.update(met.__name__, met(output, target))
+
+#             if batch_idx % self.log_step == 0:
+#                 self.logger.debug('Train Epoch: {} {} Loss: {:.6f}'.format(
+#                     epoch,
+#                     self._progress(batch_idx),
+#                     loss.item()))
+#                 self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+
+#             if batch_idx == self.len_epoch:
+#                 break
+#         log = self.train_metrics.result()
+
+#         if self.do_validation:
+#             val_log = self._valid_epoch(epoch)
+#             log.update(**{'val_'+k : v for k, v in val_log.items()})
+
+#         if self.lr_scheduler is not None:
+#             self.lr_scheduler.step()
+#         return log
+
+#     def _valid_epoch(self, epoch):
+#         """
+#         Validate after training an epoch
+
+#         :param epoch: Integer, current training epoch.
+#         :return: A log that contains information about validation
+#         """
+#         self.model.eval()
+#         self.valid_metrics.reset()
+#         with torch.no_grad():
+#             for batch_idx, (data, target) in enumerate(self.valid_data_loader):
+#                 data, target = data.to(self.device), target.to(self.device)
+
+#                 output = self.model(data)
+#                 loss = self.criterion(output, target)
+
+#                 self.writer.set_step((epoch - 1) * len(self.valid_data_loader) + batch_idx, 'valid')
+#                 self.valid_metrics.update('loss', loss.item())
+#                 for met in self.metric_ftns:
+#                     self.valid_metrics.update(met.__name__, met(output, target))
+#                 self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+
+#         # add histogram of model parameters to the tensorboard
+#         for name, p in self.model.named_parameters():
+#             self.writer.add_histogram(name, p, bins='auto')
+#         return self.valid_metrics.result()
+
+#     def _progress(self, batch_idx):
+#         base = '[{}/{} ({:.0f}%)]'
+#         if hasattr(self.data_loader, 'n_samples'):
+#             current = batch_idx * self.data_loader.batch_size
+#             total = self.data_loader.n_samples
+#         else:
+#             current = batch_idx
+#             total = self.len_epoch
+#         return base.format(current, total, 100.0 * current / total)
 
 class Trainer(BaseTrainer):
     """
     Trainer class
     """
-    def __init__(self, model, criterion, metric_ftns, optimizer, config, device,
-                 data_loader, valid_data_loader=None, lr_scheduler=None, len_epoch=None):
-        super().__init__(model, criterion, metric_ftns, optimizer, config)
-        self.config = config
+    def __init__(self, model, criterion, optimizer, config, 
+                 device=None, train_dataloader=None, valid_dataloader=None, 
+                 dataset_mean=None, dataset_std=None, lr_scheduler=None):
+        super().__init__(model, criterion, optimizer, config)
         self.device = device
-        self.data_loader = data_loader
-        if len_epoch is None:
-            # epoch-based training
-            self.len_epoch = len(self.data_loader)
-        else:
-            # iteration-based training
-            self.data_loader = inf_loop(data_loader)
-            self.len_epoch = len_epoch
-        self.valid_data_loader = valid_data_loader
-        self.do_validation = self.valid_data_loader is not None
+        self.train_dataloader = train_dataloader
+        self.valid_dataloader = valid_dataloader
+        self.dataset_mean = dataset_mean
+        self.dataset_std = dataset_std
+        self.do_validation = self.valid_dataloader is not None
         self.lr_scheduler = lr_scheduler
-        self.log_step = int(np.sqrt(data_loader.batch_size))
+        self.best_val_acc = 0
+        self.best_val_loss = np.inf
 
-        self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
-        self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
+        self.save_dir = self.increment_path(os.path.join(self.config.model_dir, self.config.name))
+        # logging
+        self.logger = SummaryWriter(log_dir=self.save_dir)
+        with open(os.path.join(self.save_dir, "config.json"), "w", encoding="utf-8") as f:
+            json.dump(vars(config), f, ensure_ascii=False, indent=4)
+
+    def increment_path(self, path, exist_ok=False):
+        """Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+
+        Args:
+            path (str or pathlib.Path): f"{model_dir}/{args.name}".
+            exist_ok (bool): whether increment path (increment if False).
+        """
+        path = Path(path)
+        if (path.exists() and exist_ok) or (not path.exists()):
+            return str(path)
+        else:
+            dirs = glob.glob(f"{path}*")
+            matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+            i = [int(m.groups()[0]) for m in matches if m]
+            n = max(i) + 1 if i else 2
+            return f"{path}{n}"
+
+    def get_lr(self, optimizer):
+        for param_group in optimizer.param_groups:
+            return param_group["lr"]
 
     def _train_epoch(self, epoch):
         """
         Training logic for an epoch
 
         :param epoch: Integer, current training epoch.
-        :return: A log that contains average loss and metric in this epoch.
         """
         self.model.train()
-        self.train_metrics.reset()
-        for batch_idx, (data, target) in enumerate(self.data_loader):
-            data, target = data.to(self.device), target.to(self.device)
-
+        loss_value = 0
+        matches = 0
+        
+        for idx, train_batch in enumerate(self.train_dataloader):
             self.optimizer.zero_grad()
-            output = self.model(data)
-            loss = self.criterion(output, target)
+
+            if self.config.multi_head:
+                inputs, labels, mask, gender, age = train_batch
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+                mask = mask.to(self.device)
+                gender = gender.to(self.device)
+                age = age.to(self.device)
+
+                outs = self.model(inputs)
+                pred_mask, pred_gender, pred_age = outs
+
+                loss_mask = self.criterion(pred_mask, mask)
+                loss_gender = self.criterion(pred_gender, gender)
+                loss_age = self.criterion(pred_age, age)
+                loss = loss_mask + loss_gender + loss_age
+                preds = torch.argmax(pred_mask, dim=-1) * 6 + torch.argmax(pred_gender, dim=-1) * 3 + torch.argmax(pred_age, dim=-1)
+
+            else:
+                inputs, labels = train_batch
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+
+                outs = self.model(inputs)
+                loss = self.criterion(outs, labels)
+
+                preds = torch.argmax(outs, dim=-1)
+
             loss.backward()
             self.optimizer.step()
+            
+            loss_value += loss.item()
+            matches += (preds == labels).sum().item()
+            if (idx + 1) % self.config.log_interval == 0:
+                train_loss = loss_value / self.config.log_interval
+                train_acc = matches / self.config.batch_size / self.config.log_interval
+                current_lr = self.get_lr(self.optimizer)
+                print(
+                    f"Epoch[{epoch}/{self.config.epochs}]({idx + 1}/{len(self.train_dataloader)}) || "
+                    f"training loss {train_loss:4.4} || training accuracy {train_acc:4.2%} || lr {current_lr}"
+                )
+                self.logger.add_scalar(
+                    "Train/loss", train_loss, epoch * len(self.train_dataloader) + idx
+                )
+                self.logger.add_scalar(
+                    "Train/accuracy", train_acc, epoch * len(self.train_dataloader) + idx
+                )
 
-            self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
-            self.train_metrics.update('loss', loss.item())
-            for met in self.metric_ftns:
-                self.train_metrics.update(met.__name__, met(output, target))
-
-            if batch_idx % self.log_step == 0:
-                self.logger.debug('Train Epoch: {} {} Loss: {:.6f}'.format(
-                    epoch,
-                    self._progress(batch_idx),
-                    loss.item()))
-                self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
-
-            if batch_idx == self.len_epoch:
-                break
-        log = self.train_metrics.result()
-
-        if self.do_validation:
-            val_log = self._valid_epoch(epoch)
-            log.update(**{'val_'+k : v for k, v in val_log.items()})
+                loss_value = 0
+                matches = 0   
 
         if self.lr_scheduler is not None:
             self.lr_scheduler.step()
-        return log
+
+        if self.do_validation:
+            self._valid_epoch(epoch)
+
 
     def _valid_epoch(self, epoch):
         """
         Validate after training an epoch
 
         :param epoch: Integer, current training epoch.
-        :return: A log that contains information about validation
         """
+        
         self.model.eval()
-        self.valid_metrics.reset()
+        
         with torch.no_grad():
-            for batch_idx, (data, target) in enumerate(self.valid_data_loader):
-                data, target = data.to(self.device), target.to(self.device)
+            print("Calculating validation results...")
+            val_loss_items = []
+            val_acc_items = []
+            figure = None
 
-                output = self.model(data)
-                loss = self.criterion(output, target)
+            for val_batch in self.valid_dataloader:
+                if self.config.multi_head:
+                    inputs, labels, mask, gender, age = val_batch
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
+                    mask = mask.to(self.device)
+                    gender = gender.to(self.device)
+                    age = age.to(self.device)
 
-                self.writer.set_step((epoch - 1) * len(self.valid_data_loader) + batch_idx, 'valid')
-                self.valid_metrics.update('loss', loss.item())
-                for met in self.metric_ftns:
-                    self.valid_metrics.update(met.__name__, met(output, target))
-                self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+                    outs = self.model(inputs)
+                    pred_mask, pred_gender, pred_age = outs
 
-        # add histogram of model parameters to the tensorboard
-        for name, p in self.model.named_parameters():
-            self.writer.add_histogram(name, p, bins='auto')
-        return self.valid_metrics.result()
+                    loss_mask = self.criterion(pred_mask, mask)
+                    loss_gender = self.criterion(pred_gender, gender)
+                    loss_age = self.criterion(pred_age, age)
+                    loss_item = (loss_mask + loss_gender + loss_age).item()
+                    preds = torch.argmax(pred_mask, dim=-1) * 6 + torch.argmax(pred_gender, dim=-1) * 3 + torch.argmax(pred_age, dim=-1)
+
+                else:
+                    inputs, labels = val_batch
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
+
+                    outs = self.model(inputs)
+                    preds = torch.argmax(outs, dim=-1)
+
+                    loss_item = self.criterion(outs, labels).item()
+
+                acc_item = (labels == preds).sum().item()
+                val_loss_items.append(loss_item)
+                val_acc_items.append(acc_item)
+
+                if figure is None:
+                    inputs_np = (
+                        torch.clone(inputs).detach().cpu().permute(0, 2, 3, 1).numpy()
+                    )
+                    inputs_np = self.denormalize_image(
+                        inputs_np, self.dataset_mean, self.dataset_std
+                    )
+                    figure = self.grid_image(
+                        inputs_np,
+                        labels,
+                        preds,
+                        n=16,
+                        shuffle=self.config.dataset != "MaskSplitByProfileDataset",
+                    )
+
+            val_loss = np.sum(val_loss_items) / len(self.valid_dataloader)
+            val_acc = np.sum(val_acc_items) / (len(self.valid_dataloader) * self.config.valid_batch_size)
+            self.best_val_loss = min(self.best_val_loss, val_loss)
+            if val_acc > self.best_val_acc:
+                print(
+                    f"New best model for val accuracy : {val_acc:4.2%}! saving the best model.."
+                )
+                
+                torch.save(self.model.module.state_dict(), f"{self.save_dir}/best.pth")
+            if val_acc > self.best_val_acc:
+                self.best_val_acc = val_acc
+            torch.save(self.model.module.state_dict(), f"{self.save_dir}/last.pth")
+            print(
+                f"[Val] acc : {val_acc:4.2%}, loss: {val_loss:4.2} || "
+                f"best acc : {self.best_val_acc:4.2%}, best loss: {self.best_val_loss:4.2}"
+            )
+            self.logger.add_scalar("Val/loss", val_loss, epoch)
+            self.logger.add_scalar("Val/accuracy", val_acc, epoch)
+            self.logger.add_figure("results", figure, epoch)
+            print()
 
     def _progress(self, batch_idx):
         base = '[{}/{} ({:.0f}%)]'
@@ -108,3 +324,55 @@ class Trainer(BaseTrainer):
             current = batch_idx
             total = self.len_epoch
         return base.format(current, total, 100.0 * current / total)
+    
+    def denormalize_image(self, image, mean, std):
+        """정규화된 이미지를 원래대로 되돌리는 메서드"""
+        img_cp = image.copy()
+        img_cp *= std
+        img_cp += mean
+        img_cp *= 255.0
+        img_cp = np.clip(img_cp, 0, 255).astype(np.uint8)
+        return img_cp
+
+    def decode_multi_class(self, multi_class_label,):
+        """인코딩된 다중 라벨을 각각의 라벨로 디코딩하는 메서드"""
+        mask_label = (multi_class_label // 6) % 3
+        gender_label = (multi_class_label // 3) % 2
+        age_label = multi_class_label % 3
+        return mask_label, gender_label, age_label
+
+    def grid_image(self, np_images, gts, preds, n=16, shuffle=False):
+        batch_size = np_images.shape[0]
+        assert n <= batch_size
+
+        choices = random.choices(range(batch_size), k=n) if shuffle else list(range(n))
+        figure = plt.figure(
+            figsize=(12, 18 + 2)
+        )  # cautions: hardcoded, 이미지 크기에 따라 figsize 를 조정해야 할 수 있습니다. T.T
+        plt.subplots_adjust(
+            top=0.8
+        )  # cautions: hardcoded, 이미지 크기에 따라 top 를 조정해야 할 수 있습니다. T.T
+        n_grid = int(np.ceil(n**0.5))
+        tasks = ["mask", "gender", "age"]
+        for idx, choice in enumerate(choices):
+            gt = gts[choice].item()
+            pred = preds[choice].item()
+            image = np_images[choice]
+            gt_decoded_labels = self.decode_multi_class(gt)
+            pred_decoded_labels = self.decode_multi_class(pred)
+            title = "\n".join(
+                [
+                    f"{task} - gt: {gt_label}, pred: {pred_label}"
+                    for gt_label, pred_label, task in zip(
+                        gt_decoded_labels, pred_decoded_labels, tasks
+                    )
+                ]
+            )
+
+            plt.subplot(n_grid, n_grid, idx + 1, title=title)
+            plt.xticks([])
+            plt.yticks([])
+            plt.grid(False)
+            plt.imshow(image, cmap=plt.cm.binary)
+
+        return figure
