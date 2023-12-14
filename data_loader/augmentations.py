4ca8a910fc54e038d7a6791efeb8408ee9fe3047
solee328 <solee328@gmail.com>
2023-12-14 04:07:00 +0000

    feat: add data_loader module

---
+++ data_loader/augmentations.py
@@ -0,0 +1,82 @@
+import torch
+from PIL import Image
+from torchvision.transforms import (
+    Resize,
+    ToTensor,
+    Normalize,
+    Compose,
+    CenterCrop,
+    ColorJitter,
+    RandomHorizontalFlip,
+)
+
+
+class BaseAugmentation:
+    """
+    기본적인 Augmentation을 담당하는 클래스
+
+    Attributes:
+        transform (Compose): 이미지를 변환을 위한 torchvision.transforms.Compose 객체
+    """
+
+    def __init__(self, resize, mean, std, **args):
+        """
+        Args:
+            resize (tuple): 이미지의 리사이즈 대상 크지
+            mean (tuple): Normalize 변환을 위한 평균 값
+            std (tuple): Normalize 변환을 위한 표준 값
+        """
+        self.transform = Compose(
+            [
+                Resize(resize, Image.BILINEAR),
+                ToTensor(),
+                Normalize(mean=mean, std=std),
+                RandomHorizontalFlip(0.5),
+            ]
+        )
+
+    def __call__(self, image):
+        """
+        이미지에 저장된 transform 적용
+
+        Args:
+            Image (PIL.Image): Augumentation을 적용할 이미지
+
+        Returns:
+            Tensor: Argumentation이 적용된 이미지
+        """
+        return self.transform(image)
+
+class AddGaussianNoise(object):
+    """이미지에 Gaussian Noise를 추가하는 클래스"""
+
+    def __init__(self, mean=0.0, std=1.0):
+        self.std = std
+        self.mean = mean
+
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+
+    def __repr__(self):
+        return self.__class__.__name__ + "(mean={0}, std={1})".format(
+            self.mean, self.std
+        )
+
+
+class CustomAugmentation:
+    """커스텀 Augmentation을 담당하는 클래스"""
+
+    def __init__(self, resize, mean, std, **args):
+        self.transform = Compose(
+            [
+                CenterCrop((320, 256)),
+                Resize(resize, Image.BILINEAR),
+                ColorJitter(0.1, 0.1, 0.1, 0.1),
+                ToTensor(),
+                Normalize(mean=mean, std=std),
+                AddGaussianNoise(),
+            ]
+        )
+
+    def __call__(self, image):
+        return self.transform(image)
--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,16 @@
-from torchvision import datasets, transforms
-from base import BaseDataLoader
+from base.base_data_loader import BaseDataLoader
 
 
-class MnistDataLoader(BaseDataLoader):
+class MaskDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+    def __init__(self, dataset, batch_size, shuffle=True, num_workers=0, pin_memory=True, drop_last=True):
+        # super().__init__(dataset, batch_size, shuffle, num_workers, drop_last, pin_memory)
+        super().__init__()
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+        self.drop_last = drop_last
