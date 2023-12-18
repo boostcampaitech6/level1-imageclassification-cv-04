9b7d00abae08eeb9b9a32dd05a9c0aff1250cfa5
H <whtjdgur77@gmail.com>
2023-12-18 12:05:59 +0900

    test.csh

--- model/loss.py
+++ model/loss.py
@@ -8,23 +8,19 @@ from torch.autograd import Variable
 # 이는 불균형한 데이터셋에서 사용되며, 잘못 분류된 샘플에 더 많은 중요도를 부여한다.
 # https://discuss.pytorch.org/t/is-this-a-correct-implementation-for-focal-loss-in-pytorch/43327/8
 class FocalLoss(nn.Module):
-    def __init__(self, num_classes=18, weight=None, gamma=2.0, reduction="mean"):
+    def __init__(self, weight=None, gamma=2.0, reduction="mean"):
         nn.Module.__init__(self)
-        if weight is None:
-            self.weight = Variable(torch.ones(num_classes, 1))
-        else:
-            if isinstance(weight, Variable):
-                self.weight = weight
-            else:
-                self.weight = Variable(weight)
         self.gamma = gamma
         self.reduction = reduction
 
     def forward(self, input_tensor, target_tensor):
-        ids = target_tensor.view(-1, 1)
-        if input_tensor.is_cuda and not self.alpha.is_cuda:
-            self.alpha = self.alpha.cuda()
-        weight = self.weight[ids.data.view(-1)]
+        class_bin_count = target_tensor.bincount()
+        ## Ver.1
+        maximum_bin = class_bin_count.max()
+        weight = maximum_bin / class_bin_count  # 각 배치마다 하나의 클래스에 대한 데이터의 개수가 다른 클래스보다 상대적으로 작을수록 더 큰 가중치 부여
+        ## Ver.2
+        minimum_bin = class_bin_count.min()
+        weight = minimum_bin / class_bin_count  # 각 배치마다 하나의 클래스에 대한 데이터의 개수가 다른 클래스보다 상대적으로 클수록 더 작은 가중치 부여
         
         log_prob = F.log_softmax(input_tensor, dim=-1)
         prob = torch.exp(log_prob)
