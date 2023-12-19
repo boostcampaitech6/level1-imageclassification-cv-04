9089fc6b5b45548b9eb1a66e57dd3aa1bafa6317
uowol <alllhvhla@gmail.com>
2023-12-19 23:48:39 +0900

    refactor: solved age head problem
    #23

--- data_loader/data_sets.py
+++ data_loader/data_sets.py
@@ -27,16 +27,11 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
         data_dir,
         multi_head,
         use_caution,
-        target_gender=None,
-        target_mask=None,
         mean=(0.548, 0.504, 0.479),
         std=(0.237, 0.247, 0.246),
         val_ratio=0.2,
     ):
         self.indices = defaultdict(list)
-        self.target_gender = target_gender  # male, female
-        self.target_mask = target_mask      # 0(mask), 1(incorrect), 2(normal)
-        # print('\n',self.target_gender, self.target_mask,'\n')
         super().__init__(data_dir, multi_head, use_caution, mean, std, val_ratio)
 
     @staticmethod
@@ -71,8 +66,6 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
                         self.data_dir, profile, file_name
                     )  # (resized_data, 000004_male_Asian_54, mask1.jpg)
                     mask_label = self._file_names[_file_name]
-
-                    if self.target_mask and mask_label != self.target_mask: continue
 
                     id, gender, race, age = profile.split("_")
 
