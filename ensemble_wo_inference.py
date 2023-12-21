fc170e3b21fa1021cf21f6aaf3ea6f19a1312ca0
cmj5064 <duttu5064@naver.com>
2023-12-21 15:09:16 +0900

    feat: code for ensemble model predictions with csv

---
+++ ensemble_wo_inference.py
@@ -0,0 +1,76 @@
+import argparse
+import os
+import pandas as pd
+from PIL import Image
+from tqdm import tqdm
+
+import data_loader.data_loaders as module_data
+import model.loss as module_loss
+import model.metric as module_metric
+import model.model as module_arch
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.transforms import Resize, ToTensor, Normalize, RandomHorizontalFlip, CenterCrop
+import numpy as np
+import glob
+
+def decode_pred(mask,gender,age):
+    mask_dict = { 0:"MASK", 1:"INCORRECT", 2:"NORMAL"}
+    gender_dict = {0:"MALE", 1:"FEMALE"}
+    age_dict = {0:'YOUNG',1:'MIDDLE',2:'OLD'}
+    
+    return mask_dict[mask],gender_dict[gender],age_dict[age]
+    
+
+def main(config):
+    # meta 데이터와 이미지 경로를 불러옵니다.
+    submission = pd.read_csv(os.path.join(config.test_dir, 'info.csv'))
+    # image_dir = os.path.join(config.test_dir, 'images')
+    # image_paths = [os.path.join(image_dir, img_id) for img_id in submission.ImageID]
+
+    # submission path로부터 submissions 리스트 생성
+    submissions = glob.glob(f'{config.submissions_dir}/*.csv')
+
+    if config.ensemble == 'hard':
+        models = []
+        hvs = []
+        for s in submissions:
+            run_name = os.path.splitext(os.path.basename(s)[11:])[0]
+
+            models.append(run_name)
+            df = pd.read_csv(s)
+            all_predictions = df['ans'].values
+            hvs.append(all_predictions)
+
+        df_hard_voting = pd.DataFrame.from_dict({k:v for k, v in zip(models, hvs)})
+        ensemble_hard_predictions = np.asarray(df_hard_voting.mode(axis=1)[0])
+
+        submission['ans'] = ensemble_hard_predictions
+
+        # 제출할 파일을 저장합니다.
+        if not os.path.exists(os.path.join(config.submissions_dir, '/ensemble_hv')):
+            os.mkdir(os.path.join(config.submissions_dir, '/ensemble_hv'))
+        submission.to_csv(f'{config.submissions_dir}/ensemble_hv/submission.csv', index=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch Template')
+    parser.add_argument(
+        "--ensemble", type=str, default="hard", help="ensemble type (default: hard)"
+    )
+    parser.add_argument(
+        "--test_dir",
+        type=str,
+        default=os.environ.get("SM_CAHNNEL_EVAL", "/data/ephemeral/home/level1-imageclassification-cv-04/data/eval")
+    )
+    parser.add_argument(
+        "--submissions_dir",
+        type=str,
+        default=os.environ.get("SM_CAHNNEL_EVAL", "/data/ephemeral/home/level1-imageclassification-cv-04/code/submissions")
+    )
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
